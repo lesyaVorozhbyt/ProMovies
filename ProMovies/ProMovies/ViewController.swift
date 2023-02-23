0d26e9a0ea108f1cdc716a20c7fc460304c8ab7b
anton8775 <yacenko372o@gmail.com>
2023-02-23 19:45:48 +0200

    Initial commit

---
+++ ProMovies/ProMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProMovies
+//
+//  Created by Антон Яценко on 23.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
