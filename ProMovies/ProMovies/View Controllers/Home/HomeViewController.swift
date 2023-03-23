66fe234f1e57f54b4877650aeeac3375733f485f
Daniel Vysochanskyi <daniel.vysochanskiy@gmail.com>
2023-03-23 21:50:59 +0200

    Added collection view, segment control, now title, colors

---
+++ ProMovies/ProMovies/View Controllers/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  ProMovies
+//
+//  Created by Test on 23.03.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Star Movie"
+    }
+
+}
