6f3169a28aeaccc4d5888c7b73765900caf6deb4
LeraCuk <LeraCuk>
2023-04-05 15:14:05 +0300

    add UICollectionViewCell in NowShowing, settings 2 Cell in 1 Row, add Items in Cell

---
+++ ProMovies/ProMovies/View Controllers/Home/NowShowingCell/NowShowingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NowShowingTableViewCell.swift
+//  ProMovies
+//
+//  Created by Lera Cuk on 03.04.2023.
+//
+
+import UIKit
+
+class NowShowingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
